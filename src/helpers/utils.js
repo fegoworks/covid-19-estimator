dbb7f09b3a2eb13fcf30334e4636c1c32fd599d8
unknown <fegoworks@hotmail.com>
2020-04-08 00:10:49 +0100

    feat(utils): create function to calculate infections by requested time

---
+++ src/helpers/utils.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/prefer-default-export */
+import Big from 'big.js';
+
+export const infectionsByRequestedTime = (data, infected) => {
+  let infections = null;
+  const {
+    timeToElapse,
+    periodType
+  } = data;
+
+  const period = Big(timeToElapse);
+  let days;
+  let unitsPerPeriod;
+
+  if (periodType === 'weeks') {
+    days = period * 7;
+    unitsPerPeriod = Math.round(days / 3);
+    infections = infected * 2 ** unitsPerPeriod;
+  } else if (periodType === 'months') {
+    days = period * 30;
+    unitsPerPeriod = Math.round(days / 3);
+    infections = infected * 2 ** unitsPerPeriod;
+  } else {
+    unitsPerPeriod = Math.round(period / 3);
+    infections = infected * 2 ** unitsPerPeriod;
+  }
+  return infections;
+};
