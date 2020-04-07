d5235523fcaf1fff0eee08802402364cd701381d
unknown <fegoworks@hotmail.com>
2020-04-08 00:13:03 +0100

    feat(estimator): calculate infections by requested time

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,28 @@
-const covid19ImpactEstimator = (data) => data;
+import Big from 'big.js';
+import infectionsByRequestedTime from './helpers/utils';
+
+const covid19ImpactEstimator = (data) => {
+  const impact = {};
+  const severeImpact = {};
+  let {
+    reportedCases
+  } = data;
+
+  reportedCases = Big(reportedCases);
+
+  impact.currentlyInfected = reportedCases * Big(10);
+  severeImpact.currentlyInfected = reportedCases * Big(50);
+
+  const impactCurrentlyInfected = Big(impact.currentlyInfected);
+  impact.infectionsByRequestedTime = infectionsByRequestedTime(data, impactCurrentlyInfected);
+  const severeImpactCurrentlyInfected = Big(severeImpact.currentlyInfected);
+  severeImpact.infectionsByRequestedTime = infectionsByRequestedTime(data, severeImpactCurrentlyInfected);
+
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
