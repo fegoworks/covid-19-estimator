30c7c7e9ccfe6c839782430641229197644c6308
unknown <fegoworks@hotmail.com>
2020-04-12 23:39:03 +0100

    feat(helpers): create saveFiles, timeInMilliseconds functions

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 export const iCases = (data, num) => data.reportedCases * num;
 
 export const numbersInfected = (data, num) => data.currentlyInfected * num;
@@ -23,4 +25,19 @@ export const getPeriod = (periodType, timeToElapse) => {
     default:
       return 'Invalid period type';
   }
+};
+
+export const timeInMilliseconds = (startTime) => {
+  const NS_PER_SEC = 1e9; // time in nano seconds
+  const NS_TO_MS = 1e6; // time in milli seconds
+  const timeDifference = process.hrtime(startTime);
+  return (timeDifference[0] * NS_PER_SEC + timeDifference[1]) / NS_TO_MS;
+};
+
+export const saveToFile = (data, filename) => {
+  fs.appendFile(filename, `${data}\n`, (err) => {
+    if (err) {
+      throw new Error('The data could not be saved');
+    }
+  });
 };
