5766877c81a770c2c6023eba46584db4400c3c82
unknown <fegoworks@hotmail.com>
2020-04-12 15:57:54 +0100

    feat(validator): validate user input

--- src/routes/estimate.route.js
+++ src/routes/estimate.route.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import validate from '../middleware/validator';
 import estimateController from '../controllers/estimate.controller';
 
 const router = express.Router();
 
-router.post('/on-covid-19/', estimateController.covidEstimate);
+router.post('/on-covid-19/',
+  validate.validateBody(validate.schemas.paramSchema),
+  estimateController.covidEstimate);
 
 export default router;
